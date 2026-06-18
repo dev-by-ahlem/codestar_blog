0ca345c5f07dea3e0e27c0970c87bbe7fb952061
dev-by-ahlem <br-ahlem@hotmail.com>
2026-06-19 10:43:18 +1300

    Add excerpt field to Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h0jkbwgw$(yd&%&6+al5ip@g8keedv@7y(q-+ud7zxg+nw5p3t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
