7aac7f24d8b6c05894e8fdc1aeac4b564d1b815b
dev-by-ahlem <br-ahlem@hotmail.com>
2026-06-22 04:03:06 +1300

    Add homepage view with generic ListView

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
 from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('', views.blog_home, name='blog-home'),
+    path('', views.PostList.as_view(), name='home'),
 ]
