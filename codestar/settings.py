97efa4015b812797c3cbcb45ae1b9c05bc14865a
dev-by-ahlem <br-ahlem@hotmail.com>
2026-06-24 08:15:34 +1300

    add post detail view and template

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-h0jkbwgw$(yd&%&6+al5ip@g8keedv@7y(q-+ud7zxg+nw5p3t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
