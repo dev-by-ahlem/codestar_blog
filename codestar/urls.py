5fd0a465fc901311b10b6b57586ba01dc3684212
dev-by-ahlem <br-ahlem@hotmail.com>
2026-06-25 01:12:20 +1300

    Add about app

--- codestar/urls.py
+++ codestar/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', include('about.urls')),
+    path('', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
-    path("about/", include("about.urls"), name="about-urls"),
-    path("", include("blog.urls"), name="blog-urls"),
 ]
