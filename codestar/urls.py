7dd5b370be87f95f15ff08de2c1d313837f76103
dev-by-ahlem <br-ahlem@hotmail.com>
2026-06-25 02:28:49 +1300

    added authentication

--- codestar/urls.py
+++ codestar/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', include('about.urls')),
-    path('', include('blog.urls')),
+    path('accounts/', include('allauth.urls')),   # ⭐ ADD THIS
     path('summernote/', include('django_summernote.urls')),
+    path('', include('blog.urls')),
 ]
